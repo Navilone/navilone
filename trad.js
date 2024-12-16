4a740a81503d0c9d49659ff8e7d6c65a7bc040c6
Navilone <prasath.thamboo@navilone.com>
2024-12-16 11:00:28 +0100

    Sixth (Trad)

--- trad.js
+++ trad.js
@@ -1,25 +1,35 @@
 const translations = {
     en: {
       home:"Home",
-      about: "About",
+      about:"About",
       news:"News", 
+      welcome:"Welcome to the website of",
+      expert:"We are expert in",
+      contacten:"Contact us", 
+
     },
     
     fr: {
       home:"Accueil",
       about: "A propos",
-      news:"Articles"
-
+      news:"Articles",
+      welcome:"Bienvenue sur le site de",
+      expert:"Nous sommes experts en",
+      contacten:"Contactez-moi",
+        
     },
   };
   
   function setLanguage(lang) {
-    document.getElementById('home').innerText = translations[lang].home;
+    document.getElementById('homee').innerText = translations[lang].home;
     document.getElementById('about').innerText = translations[lang].about;
     document.getElementById('news').innerText = translations[lang].news;
+    document.getElementById('welcome').innerText = translations[lang].welcome;
+    document.getElementById('expert').innerText = translations[lang].expert;
+    document.getElementById('contacten').innerText = translations[lang].contacten;
+
 
   }
   
   // Langue par défaut
   setLanguage('en');
-  
