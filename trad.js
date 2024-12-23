ae16a9da9c013197318679039f4c01977e0872da
Navilone <prasath.thamboo@navilone.com>
2024-12-23 11:36:48 +0100

    Update trad.js

--- trad.js
+++ trad.js
@@ -6,12 +6,32 @@ const translations = {
         welcome:"Welcome to the website of",
         expert:"We are expert in",
         contacten:"Contact us", 
-        art1tit:"Business advisory",
-        art1tex:"We provide consulting services in project and management that fulfill your company's needs",
-        art2tit:"Management tools",
-        art2tex:"We help you manage your projects through cutting edge technologies such as Power Platfom and Dynamics 365",
-        art3tit:"Trainings",
-        art3tex:"We train and support your teams using our own methodologies",
+        valtitle :"Our values",
+        val1tit:"Business advisory",
+        val1tex:"We provide consulting services in project and management that fulfill your company's needs",
+        val2tit:"Management tools",
+        val2tex:"We help you manage your projects through cutting edge technologies such as Power Platfom and Dynamics 365",
+        val3tit:"Trainings",
+        val3tex:"We train and support your teams using our own methodologies",
+        arttitle:"Our important articles",
+        arttit1:"Optimize your business management with Microsoft Business Central",
+        arttit2:"Choose Dynamics 365 for customer relationship management (CRM)",
+        arttit3:"Enhance your processes with Power Platform",
+        arttit4:"Improve your performance by going digital",
+        arttit5:"Use the power of data analysis with Power BI",
+        arttit6:"Integrate Microsoft 365 seamlessly with your business systems",
+        arttex1:"Discover how Microsoft Business Central can simplify your business management by centralizing all your processes in a single platform. This solution enables you to automate tasks, improve financial visibility and make decisions based on real-time data.
+",
+        arttex2:"Choosing Dynamics 365 for customer relationship management (CRM) means opting for a complete solution that unifies sales, marketing and customer service. Gain a 360° view of your customers, automate tasks, personalize interactions and make decisions based on real-time data to improve engagement and loyalty.
+",
+        arttex3:"nhance your processes with Power Platform, a suite of Microsoft tools for creating customized applications, automating repetitive tasks and analyzing data. Easily connect your different information sources, optimize your workflows and gain in efficiency to accelerate your company's digital transformation.
+",
+        arttex4:"Digital transformation improves your business performance by automating tasks, optimizing processes and facilitating access to real-time data. It enables you to better understand customer needs, accelerate decision-making and personalize offers, all of which increase efficiency and boost your company's growth.
+",
+        arttex5:"Discover the power of data analysis with Power BI, a Microsoft tool that transforms your data into actionable information. Create interactive dashboards, visualize trends in real time and make informed decisions to boost your performance. Simplify complex analysis and easily share insights with your team.
+",
+        arttex6:"Microsoft 365 integration with your business systems delivers a seamless experience, connecting your productivity tools to your business applications. Easily access data, collaborate in real time and automate workflows. This integration boosts efficiency, reduces silos and optimizes your operations.
+",
 
     },    
     
@@ -22,13 +42,26 @@ const translations = {
         welcome:"Bienvenue sur le site de",
         expert:"Nous sommes experts en",
         contacten:"Contactez-moi",
-        art1tit:"Conseil pour PME",
-        art1tex:"Nous proposons des services de conseil en gestion de projet qui répondent aux besoins spécifiques de votre entreprise.",
-        art2tit:"Outils de gestion",
-        art2tex:"Nous mettons en place les outils nécessaires pour la gestion de vos projets avec Powerplateform et Dynamics365.",
-        art3tit:"Formations",
-        art3tex:"Nous formons et suivons les collaborateurs pour utiliser les outils de manière optimales que nous vous avons mis en place.",
-
+        valtitle :"Nos valeurs",
+        val1tit:"Conseil pour PME",
+        val1tex:"Nous proposons des services de conseil en gestion de projet qui répondent aux besoins spécifiques de votre entreprise.",
+        val2tit:"Outils de gestion",
+        val2tex:"Nous mettons en place les outils nécessaires pour la gestion de vos projets avec Powerplateform et Dynamics365.",
+        val3tit:"Formations",
+        val3tex:"Nous formons et suivons les collaborateurs pour utiliser les outils de manière optimales que nous vous avons mis en place.",
+        arttitle:"Nos articles importants",
+        arttit1:"Optimisez votre gestion d'entreprise avec Microsoft Business Central",
+        arttit2:"Choisissez Dynamics 365 pour la gestion de relation client (CRM)",
+        arttit3:"Améliorez vos processus grâce à Power Platform",
+        arttit4:"Améliorez vos performances en la digitalisant ",
+        arttit5:"Utilisez la puissance de l’analyse de données avec Power BI",
+        arttit6:"Intégrez Microsoft 365 de manière transparente à vos systèmes d'entreprise",
+        arttex1:" Découvrez comment Microsoft Business Central peut simplifier la gestion de votre entreprise en centralisant tous vos processus en une seule plateforme. Cette solution permet d'automatiser les tâches, d'améliorer la visibilité financière et de prendre des décisions basées sur des données en temps réel.",
+        arttex2:"Choisir Dynamics 365 pour la gestion de la relation client (CRM), c'est opter pour une solution complète qui unifie les ventes, le marketing et le service client. Bénéficiez d'une vue à 360° sur vos clients, automatisez les tâches, personnalisez les interactions et prenez des décisions basées sur des données en temps réel pour améliorer l'engagement et la fidélisation.",
+        arttex3:"Améliorez vos processus avec Power Platform, une suite d'outils Microsoft qui permet de créer des applications personnalisées, d'automatiser les tâches répétitives et d'analyser les données. Connectez facilement vos différentes sources d'information, optimisez vos flux de travail et gagnez en efficacité pour accélérer la transformation digitale de votre entreprise.",
+        arttex4:"La transformation digitale améliore vos performances commerciales en automatisant les tâches, optimisant les processus et facilitant l'accès aux données en temps réel. Elle permet de mieux comprendre les besoins des clients, d'accélérer la prise de décision et de personnaliser les offres, ce qui augmente l'efficacité et booste la croissance de votre entreprise.",
+        arttex5:"Découvrez la puissance de l'analyse de données avec Power BI, un outil Microsoft qui transforme vos données en informations exploitables. Créez des tableaux de bord interactifs, visualisez les tendances en temps réel et prenez des décisions éclairées pour booster vos performances. Simplifiez l'analyse complexe et partagez facilement les insights avec votre équipe.",
+        arttex6:"L'intégration de Microsoft 365 avec vos systèmes d'entreprise offre une expérience fluide, connectant vos outils de productivité à vos applications métier. Accédez facilement aux données, collaborez en temps réel et automatisez les flux de travail. Cette intégration renforce l'efficacité, réduit les silos et optimise vos opérations.",
         
     },
   };
@@ -40,13 +73,26 @@ const translations = {
     document.getElementById('welcome').innerText = translations[lang].welcome;
     document.getElementById('expert').innerText = translations[lang].expert;
     document.getElementById('contacten').innerText = translations[lang].contacten;
-    document.getElementById('art1tit').innerText = translations[lang].art1tit;
-    document.getElementById('art1tex').innerText = translations[lang].art1tex;
-    document.getElementById('art2tit').innerText = translations[lang].art2tit;
-    document.getElementById('art2tex').innerText = translations[lang].art2tex;
-    document.getElementById('art3tit').innerText = translations[lang].art3tit;
-    document.getElementById('art3tex').innerText = translations[lang].art3tex;
-      
+    document.getElementById('valtitle').innerText = translations[lang].valtitle;
+    document.getElementById('val1tit').innerText = translations[lang].val1tit;
+    document.getElementById('val1tex').innerText = translations[lang].val1tex;
+    document.getElementById('val2tit').innerText = translations[lang].val2tit;
+    document.getElementById('val2tex').innerText = translations[lang].val2tex;
+    document.getElementById('val3tit').innerText = translations[lang].val3tit;
+    document.getElementById('val3tex').innerText = translations[lang].val3tex;
+    document.getElementById('arttitle').innerText = translations[lang].arttitle;
+    document.getElementById('arttit1').innerText = translations[lang].arttit1;
+    document.getElementById('arttit2').innerText = translations[lang].arttit2;
+    document.getElementById('arttit3').innerText = translations[lang].arttit3;
+    document.getElementById('arttit4').innerText = translations[lang].arttit4;
+    document.getElementById('arttit5').innerText = translations[lang].arttit5;
+    document.getElementById('arttit6').innerText = translations[lang].arttit6;
+    document.getElementById('arttex1').innerText = translations[lang].arttex1;
+    document.getElementById('arttex2').innerText = translations[lang].arttex2;
+    document.getElementById('arttex3').innerText = translations[lang].arttex3;
+    document.getElementById('arttex4').innerText = translations[lang].arttex4;
+    document.getElementById('arttex5').innerText = translations[lang].arttex5;
+    document.getElementById('arttex6').innerText = translations[lang].arttex6;
 
 
   }
